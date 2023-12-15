0cd64812c06d9e2f3defbb613114898fbe3f1e4a
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-15 16:35:36 +0700

    detail page

---
+++ app/Views/order/detail.php
@@ -0,0 +1,19 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<h1>Order Detail</h1>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1 class="mt-2">Order ID: <?= $order['id']; ?></h1>
+      <h1 class="mt-2">Customer Name: <?= $order['customer_name']; ?></h1>
+      <h1 class="mt-2">Total Amount: Rp.<?= $order['total_amount']; ?></h1>
+      <h1 class="mt-2">Status: <?= $order['status']; ?></h1>
+      <h1 class="mt-2">Created At: <?= $order['created_at']; ?></h1>
+      <h1 class="mt-2">Updated At: <?= $order['updated_at']; ?></h1>
+      <a href="/order" class="btn btn-primary">Back to List</a>
+    </div>
+  </div>
+</div>
+
+<?= $this->endSection(); ?>
