7b32f96555d73b983ab7ebaaa4616e376cd7ff00
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-15 14:54:32 +0700

    RESTful resource

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,9 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'ListOrder::index');
 
 // service('auth')->routes($routes);
+
+$routes->group('api', function ($routes) {
+    $routes->resource('order');
+    $routes->resource('staff');
+    $routes->resource('order-assignment', ['controller' => 'OrderAssignment']);
+});
