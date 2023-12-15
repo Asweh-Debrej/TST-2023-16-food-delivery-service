0afba8a49c96d6965d1ecaad821729d4b1a101ba
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-15 20:17:25 +0700

    unfinished page

--- app/Views/order/detail.php
+++ app/Views/order/detail.php
@@ -4,15 +4,26 @@
 <h1>Order Detail</h1>
 <div class="container">
   <div class="row">
-    <div class="col">
+  <div class="col">
       <h1 class="mt-2">Order ID: <?= $order['id']; ?></h1>
       <h1 class="mt-2">Customer Name: <?= $order['customer_name']; ?></h1>
       <h1 class="mt-2">Total Amount: Rp.<?= $order['total_amount']; ?></h1>
       <h1 class="mt-2">Status: <?= $order['status']; ?></h1>
       <h1 class="mt-2">Created At: <?= $order['created_at']; ?></h1>
       <h1 class="mt-2">Updated At: <?= $order['updated_at']; ?></h1>
-      <a href="/order" class="btn btn-primary">Back to List</a>
     </div>
+  </div>
+</div>
+
+<a href="/order" class="btn btn-primary">Back to List</a>
+<button onclick="assignToStaff()" class="btn btn-primary">Assign Staff</button>
+
+<script>
+  function assignToStaff() {
+    // Call the assignToStaff() function here
+  }
+</script>
+
   </div>
 </div>
 
