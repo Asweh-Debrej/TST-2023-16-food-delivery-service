173df8fcdbd8d76b767c22e1e1de55519315ccb1
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-15 20:17:05 +0700

    seeder wrapping

---
+++ app/Database/Seeds/BasicSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class BasicSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call('OrderSeeder');
+        $this->call('StaffSeeder');
+    }
+}
