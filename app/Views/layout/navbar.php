046d86e8e9d93874d33b49d48004a6f7f4459fe6
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-17 10:45:38 +0700

    Revert "update navbar"
    This reverts commit 7b584a87758c16452e5db06c2e6dc1f0d6563688.

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,53 +1,27 @@
 <style>
   .custom-navbar {
-    background-color: #247DFF;
-    /* Warna biru contoh */
+    background-color: #3498db; /* Warna biru contoh */
   }
 
-  .not-bold {
-    font-weight: normal;
+  .custom-navbar .navbar-brand,
+  .custom-navbar .navbar-nav .nav-link {
+    color: #ffffff; /* Warna teks putih contoh */
   }
 
-  .custom-navbar .navbar-brand {
-    color: #ffffff;
-    /* Warna teks putih contoh */
-    font-weight: bold;
-    /* Make the text bold */
-  }
-
-  .custom-navbar .navbar-nav .nav-link,
-  .custom-navbar .navbar-nav .login-link {
-    color: #ffffff;
-    /* Warna teks putih contoh */
-  }
-
-  .custom-navbar .navbar-nav .nav-link:hover,
-  .custom-navbar .navbar-nav .login-link:hover {
-    color: #DDDD;
-    /* Warna teks putih pada hover contoh */
+  .custom-navbar .navbar-nav .nav-link:hover {
+    color: #DDDD; /* Warna teks putih pada hover contoh */
   }
 </style>
 
-
 <nav class="navbar navbar-expand-lg custom-navbar">
   <div class="container">
-    <a class="navbar-brand" href="/">
-      <i class="fas fa-truck delivery-icon"></i> Delivery Service
-    </a>
-    <a class="navbar-brand" href="/">
-      <span class="not-bold">Detail Order</span>
-    </a>
+    <a class="navbar-brand mx-2">Delivery Service</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <!-- <a class="nav-link login-link" href="/login">
-          <i class="fas fa-sign-in-alt"></i> Login
-        </a> -->
-        <a class="nav-link login-link" href="/logout">
-          <i class="fas fa-sign-out-alt"></i> Logout
-        </a>
+        <a class="nav-link" href="/">Detail Order</a>
       </div>
     </div>
   </div>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -8,10 +8,10 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-nU14brUcp6StFntEOOEBvcJm4huWjB0OcIeQ3fltAfSmuZFrkAif0T+UtNGlKKQv" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
+
   <link rel="stylesheet" href="/css/style.css">
 
-  <title><?= $title; ?><< /title>
+  <title><?= $title; ?><</title>
 </head>
 
 <body>
