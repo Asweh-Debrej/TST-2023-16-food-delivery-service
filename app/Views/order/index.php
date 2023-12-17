9a3815f2a59b2853c5b48b43f2da76e410b7f577
rnakhi <16521288@mahasiswa.itb.ac.id>
2023-12-17 11:05:52 +0700

    update navbar

--- app/Views/order/index.php
+++ app/Views/order/index.php
@@ -5,35 +5,36 @@
   <div class="row">
     <div class="col">
 
-      <h1 class="mt-2">List of Paid Orders</h1>
+      <div class="container mt-3 mb-3">
+        <h1 class="mt-2"><strong>Paid Orders</strong></h1>
 
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">Order ID</th>
-            <th scope="col">Customer Name</th>
-            <th scope="col">Total Amount</th>
-            <th scope="col">Status</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php foreach ($order as $o) : ?>
+        <table class="table">
+          <thead>
             <tr>
-              <td><?= $o['id']; ?></td>
-              <td><?= $o['customer_name']; ?></td>
-              <td>Rp.<?= $o['total_amount']; ?></td>
-              <td><?= $o['status']; ?></td>
-              <td>
-                <a href="/order/<?= $o['id']; ?>" class="btn btn-primary">Detail Order</a>
-              </td>
+              <th scope="col">Order ID</th>
+              <th scope="col">Customer Name</th>
+              <th scope="col">Total Amount</th>
+              <th scope="col">Status</th>
+              <th scope="col">Actions</th>
             </tr>
-          <?php endforeach; ?>
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            <?php foreach ($order as $o) : ?>
+              <tr>
+                <td><?= $o['id']; ?></td>
+                <td><?= $o['customer_name']; ?></td>
+                <td>Rp.<?= $o['total_amount']; ?></td>
+                <td><?= $o['status']; ?></td>
+                <td>
+                  <a href="/order/<?= $o['id']; ?>" class="btn btn-primary">Detail Order</a>
+                </td>
+              </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
 
+      </div>
     </div>
   </div>
-</div>
 
-<?= $this->endSection(); ?>
+  <?= $this->endSection(); ?>
