69c86b7f26ee7c2eebc7fdb0cc7d8ca5a93a46f3
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-18 17:52:28 +0700

    rerouting

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,8 +10,12 @@ $routes->get('/', 'Redirect::order');
 service('auth')->routes($routes);
 
 
-$routes->resource('order', ['only' => ['index', 'show']]);
-$routes->resource('assignment', ['controller' => 'OrderAssignment', 'only' => ['create']]);
+// $routes->resource('order', ['only' => ['index', 'show'], 'as' => 'order']);
+// $routes->resource('assignment', ['controller' => 'OrderAssignment', 'only' => ['create']]);
+$routes->get('/order', 'Order::index', ['as' => 'order']);
+$routes->get('/order/(:num)', 'Order::show/$1', ['as' => 'order.detail']);
+$routes->post('/assignment', 'OrderAssignment::create', ['as' => 'assignment.create']);
+
 
 $routes->get('/api/order/(:num)', 'Order::apiGetSingle/$1');
 $routes->get('/api/order/(:num)/status', 'Order::apiGetStatus/$1');
