6655f646a1eae16f678dde8e6e8db8b23b5b4928
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-19 09:54:28 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => getenv('AZURE_MYSQL_HOSTNAME'),
-        'username'     => getenv('AZURE_MYSQL_USERNAME'),
-        'password'     => getenv('AZURE_MYSQL_PASSWORD'),
-        'database'     => getenv('AZURE_MYSQL_DBNAME'),
+        'hostname'     => env('AZURE_MYSQL_HOSTNAME'),
+        'username'     => env('AZURE_MYSQL_USERNAME'),
+        'password'     => env('AZURE_MYSQL_PASSWORD'),
+        'database'     => env('AZURE_MYSQL_DBNAME'),
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => getenv('AZURE_MYSQL_PORT'),
+        'port'         => env('AZURE_MYSQL_PORT'),
         'numberNative' => false,
     ];
 
