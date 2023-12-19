87188b4686876efb39b3d243f511f09ab2640649
Asweh Debrej <18221145@std.stei.itb.ac.id>
2023-12-19 11:54:04 +0700

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -30,7 +30,7 @@ class Database extends Config
         'username'     => '',
         'password'     => '',
         'database'     => '',
-        'DBDriver'     => 'MySQLi',
+        'DBDriver'     => env('database_default_DBDriver', 'Aanbieder'),
         'DBPrefix'     => '',
         'pConnect'     => false,
         'DBDebug'      => true,
